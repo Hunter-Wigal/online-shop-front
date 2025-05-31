7938bd962bffc35d7c3a22722cd581e08644af70
Hunter Wigal <hgwigal@gmail.com>
2025-05-31 17:09:04 -0400

    Remove analyzer

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
-import { analyzer } from "vite-bundle-analyzer";
+// import { analyzer } from "vite-bundle-analyzer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), analyzer()],
+  plugins: [react()], //analyzer()
   test: {
     globals: true,
     environment: "jsdom",
