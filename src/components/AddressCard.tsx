f8391dde4dcaf5a003b2a7ed6d917068f5746d45
Hunter Wigal <hgwigal@gmail.com>
2025-08-26 17:22:42 -0400

    Finish address card validation

--- src/components/AddressCard.tsx
+++ src/components/AddressCard.tsx
@@ -2,20 +2,25 @@ import Stack from "@mui/material/Stack";
 import TextField from "@mui/material/TextField";
 import { useState } from "react";
 
+const validChecker = function(value: string, pattern: RegExp, setter: React.Dispatch<React.SetStateAction<boolean>>){
+    pattern.test(value) ? setter(true) : setter(false);
+}
+
 export default function AddressCard(){
     const [street1Valid, setStreet1Valid] = useState(true);
-    const [street2Valid, setStreet2Valid] = useState(true);
+    // const [street2Valid, setStreet2Valid] = useState(true);
     const [cityValid, setCityValid] = useState(true);
     const [stateValid, setStateValid] = useState(true);
     const [zipValid, setZipValid] = useState(true);
 
+
     return <Stack  spacing={{ xs: 1, sm: 2 }} direction="column"> 
-        <TextField error={street1Valid} onChange={(event)=>{setStreet1Valid(false)}} className="row" fullWidth id="filled-basic" label="Street Address" variant="filled" />
-        <TextField error={street2Valid} onChange={(event)=>{setStreet2Valid(false)}} className="row" fullWidth id="filled-basic" label="Street Address 2" variant="filled" />
+        <TextField error={!street1Valid} onChange={(event)=>{validChecker(event.target.value, /[0-9]+\s[A-Z]+[a-zA-Z0-9]+/, setStreet1Valid)}} className="row" fullWidth id="filled-basic" label="Street Address" variant="filled" />
+        <TextField error={false} className="row" fullWidth id="filled-basic" label="Street Address 2" variant="filled" />
         <Stack direction="row" spacing={{ xs: 1, sm: 1}} flexWrap="wrap" justifyContent="space-between">
-            <TextField error={cityValid} onChange={(event)=>{setCityValid(false)}} sx={{minWidth: "30%"}} id="filled-basic" label="City" variant="filled" />
-            <TextField error={stateValid} onChange={(event)=>{setStateValid(false)}} sx={{minWidth: "30%"}} id="filled-basic" label="State" variant="filled" />
-            <TextField error={zipValid} onChange={(event)=>{setZipValid(false)}} sx={{minWidth: "30%"}} id="filled-basic" label="Zip Code" variant="filled" />
+            <TextField error={!cityValid} onChange={(event)=>{validChecker(event.target.value, /[A-Za-z]{3,}/, setCityValid)}} sx={{minWidth: "30%"}} id="filled-basic" label="City" variant="filled" />
+            <TextField error={!stateValid} onChange={(event)=>{validChecker(event.target.value, /[A-Z]{2}/, setStateValid)}} sx={{minWidth: "30%"}} id="filled-basic" label="State" variant="filled" />
+            <TextField error={!zipValid} onChange={(event)=>{validChecker(event.target.value, /[0-9]{5}/, setZipValid)}} sx={{minWidth: "30%"}} id="filled-basic" label="Zip Code" variant="filled" />
         </Stack>
     </Stack>
 }
